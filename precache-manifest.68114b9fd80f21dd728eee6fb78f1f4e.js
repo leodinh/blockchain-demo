e0edcbf2690e9830ecb9229abd7f08d5accb69fe
leodinh <NguyenAnhTuan.Dinh@georgebrown.ca>
2020-01-31 15:06:31 -0500

    Updates

---
+++ precache-manifest.68114b9fd80f21dd728eee6fb78f1f4e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79130267a3fd8dda06a4dd5fd9de7a0",
+    "url": "/blockchain-demo/610538a06ea2e2844c10.worker.js"
+  },
+  {
+    "revision": "e29cadd46dd096e0c81a19a600afc9c8",
+    "url": "/blockchain-demo/index.html"
+  },
+  {
+    "revision": "45d9c01c23a9e05283dc",
+    "url": "/blockchain-demo/static/css/main.b24a0254.chunk.css"
+  },
+  {
+    "revision": "d68eab84a8628fffb63a",
+    "url": "/blockchain-demo/static/js/2.8050ad83.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/blockchain-demo/static/js/2.8050ad83.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45d9c01c23a9e05283dc",
+    "url": "/blockchain-demo/static/js/main.2bb554ce.chunk.js"
+  },
+  {
+    "revision": "ce33ef30a52736cf5338",
+    "url": "/blockchain-demo/static/js/runtime-main.729b7c3c.js"
+  },
+  {
+    "revision": "7627687e36eb71b6e6aaf6ebaea36d07",
+    "url": "/blockchain-demo/static/media/loading.7627687e.svg"
+  },
+  {
+    "revision": "9064981b80e59f1b663c17fab41563c9",
+    "url": "/blockchain-demo/static/media/mining.9064981b.svg"
+  },
+  {
+    "revision": "4859caa7571163b1f40b75879aedc0fe",
+    "url": "/blockchain-demo/static/media/sprite.4859caa7.svg"
+  }
+]);
