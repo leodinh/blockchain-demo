7e2bbf6976d5785778d43dcf5d92aea9eba9bcb7
leodinh <NguyenAnhTuan.Dinh@georgebrown.ca>
2020-01-31 14:59:31 -0500

    Finished utility including cryptographic function

---
+++ src/shared/block_miner.js
@@ -0,0 +1,33 @@
+import { Utils } from "./utility.js";
+
+let utils = new Utils();
+let timestamp,
+  prevHash,
+  data,
+  index = null;
+
+onmessage = function(e) {
+  var obj = JSON.parse(e.data);
+  timestamp = obj.timestamp;
+  prevHash = obj.prevHash;
+  data = obj.data;
+  index = obj.index;
+  utils.setDifficulty(obj.difficulty);
+
+  createBlock();
+};
+
+function createBlock() {
+  var returnBl = {
+    timestamp: timestamp,
+    data: data,
+    previousHash: prevHash,
+    index: index
+  };
+
+  var nonce = utils.calculateNonce(returnBl);
+  returnBl["nonce"] = nonce;
+  var hash = utils.hashBlock(returnBl);
+  returnBl["hash"] = hash;
+  postMessage(returnBl);
+}
---
+++ src/shared/utility.js
@@ -0,0 +1,63 @@
+import { ethers } from "ethers";
+export const updateObject = (oldObject, updatedValues) => {
+  return {
+    ...oldObject,
+    ...updatedValues
+  };
+};
+export class Utils {
+  setDifficulty(difficulty) {
+    this.difficulty = difficulty;
+  }
+
+  hashBlock(blockToHash) {
+    let stringifiedBl = JSON.stringify(blockToHash);
+    let hexifiedBl = this.toHex(stringifiedBl);
+    return ethers.utils.keccak256(hexifiedBl);
+  }
+
+  calculateNonce(block) {
+    let clonedBlock = JSON.parse(JSON.stringify(block));
+    let nonce = 0;
+    clonedBlock["nonce"] = nonce;
+    let hash = this.hashBlock(clonedBlock);
+
+    while (
+      !(hash.substring(2, this.difficulty + 2) === this.getLeadingZeros())
+    ) {
+      nonce++;
+      clonedBlock["nonce"] = nonce;
+      hash = this.hashBlock(clonedBlock);
+    }
+    return nonce;
+  }
+
+  verifyBlock(block) {
+    var blockWithoutHash = {
+      timestamp: block.timestamp,
+      data: block.data,
+      previousHash: block.previousHash,
+      index: block.index,
+      nonce: block.nonce
+    };
+
+    var hash = this.hashBlock(blockWithoutHash);
+    return hash === block.hash;
+  }
+
+  getLeadingZeros() {
+    var result = "";
+    for (let i = 0; i < this.difficulty; i++) {
+      result = result.concat("0");
+    }
+    return result;
+  }
+
+  toHex(input) {
+    var result = "";
+    for (var i = 0; i < input.length; i++) {
+      result += input.charCodeAt(i).toString(16);
+    }
+    return "0x" + result;
+  }
+}
